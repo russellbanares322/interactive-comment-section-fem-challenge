810f873ffa83f9540e91a0ba8e78052f7f4803c7
russellbanares322 <banaresrussell11@gmail.com>
2023-04-09 11:35:23 +0800

    feat: add increment and decrement score function

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -4,6 +4,16 @@ import CommentInput from "../comment-input/CommentInput";
 
 const Comments = ({ comment }) => {
   const [showInput, setShowInput] = useState(false);
+  const [score, setScore] = useState(comment?.score);
+
+  const handleIncrementScore = () => {
+    setScore((prev) => prev + 1);
+  };
+  const handleDecrementScore = () => {
+    if (score !== 0) {
+      setScore((prev) => prev - 1);
+    }
+  };
 
   const commentsID = comment?.id;
   const replyID = comment?.replies?.map((r) => r?.id);
@@ -20,11 +30,13 @@ const Comments = ({ comment }) => {
       <div className="bg-white h-auto rounded-md mt-3 md:mt-3">
         <div className="flex justify-start items-center py-4 px-5 gap-5">
           <div className="bg-light-gray flex flex-col gap-3 h-full items-center justify-center px-2 py-3 w-8 rounded-md">
-            <PlusIcon />
-            <p className="text-moderate-blue font-medium text-sm">
-              {comment?.score}
-            </p>
-            <MinusIcon />
+            <div className="cursor-pointer" onClick={handleIncrementScore}>
+              <PlusIcon />
+            </div>
+            <p className="text-moderate-blue font-medium text-sm">{score}</p>
+            <div className="cursor-pointer" onClick={handleDecrementScore}>
+              <MinusIcon />
+            </div>
           </div>
           <div className="w-full">
             <div className="flex justify-between items-center">
@@ -55,7 +67,7 @@ const Comments = ({ comment }) => {
           </div>
         </div>
       </div>
-      <div className="relative md:before:absolute md:before:left-10 md:before:top-4 md:before:h-5/6 md:before:border-l md:before:border-light-grayish-blue flex flex-col justify-end items-end">
+      <div className="relative md:before:absolute md:before:left-10 md:before:top-7 md:before:h-5/6 md:before:border-l md:before:border-light-grayish-blue flex flex-col justify-end items-end">
         {comment?.replies?.map((comment) => (
           <div className="w-full md:w-[36rem] pl-0 md:pl-7" key={comment?.id}>
             <Comments comment={comment} />
